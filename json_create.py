909db717766d8fa5ab05b03a108dd9e37f2c4114
humo174 <wakka174@gmail.com>
2023-02-13 15:47:21 +0500

    add del user func

--- json_create.py
+++ json_create.py
@@ -5,7 +5,10 @@ data = {
     'oauth_token': "YOUR_OAUTH_TOKEN",
     'catalog_id': "YOUR_CATALOG_ID",
     'admin_id': YOUR_ID,
-    'allow_users': [USER_ID1, USER_ID2]
+    'allow_users': {
+        'USER_ID1': 'NAME_USER1',
+        'USER_ID2': 'NAME_USER2'
+    }
 }
 
 with open('creds.json', 'w', encoding='utf-8') as jo:
